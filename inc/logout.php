c090f7515b0e5be68604c19a62bee660626b69eb
starflowerbracken <starflower.bracken@gmail.com>
2021-10-11 12:03:46 +0100

    Better English again

--- inc/logout.php
+++ inc/logout.php
@@ -1,5 +1,5 @@
 <?php
-$_SESSION['flash']['success'] = "You are disconnected successfully ".$_SESSION["username"]." ...";
+$_SESSION['flash']['success'] = "You have logged out successfully, ".$_SESSION["username"]." ...";
 unset($_SESSION["valid"]);
 unset($_SESSION["username"]);
 unset($_SESSION['useruuid']);
